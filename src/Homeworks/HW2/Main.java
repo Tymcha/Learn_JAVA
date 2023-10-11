6aff07a067f27ec7c6fb666bce4c211155ac580c
Tymcha <andriyy29.timchuk@gmail.com>
2023-10-11 18:49:11 +0300

    HW2 Task_1

--- src/Homeworks/HW2/Main.java
+++ src/Homeworks/HW2/Main.java
@@ -2,21 +2,13 @@ package Homeworks.HW2;
 
 public class Main {
     public static void main(String[] args) {
-        String stringAll = "First,Second,Third,Fifth,Fourth,Sixth,Eighth,Seventh,Ninth";
-        String[] stringArray = stringAll.split (",");
-        String buff;
+        int age = 20;
+        float weight = 70f;
+        String name = "Andriy";
 
-        buff = stringArray[3];
-        stringArray[3] = stringArray[4];
-        stringArray[4] = buff;
-
-        buff = stringArray[6];
-        stringArray[6] = stringArray[7];
-        stringArray[7] = buff;
-
-        for (int i = 0; i < stringArray.length; i++) {
-            System.out.println(stringArray[i]);
-        }
+        System.out.println("Name: " + name + ",\t" +
+                            "Age: " + age + ",\t" +
+                            "Weighht: " + weight);
 
     }
 }
