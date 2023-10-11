035c9e37a4015fa7d67a9ec7105a01b4e0c808e6
Tymcha <andriyy29.timchuk@gmail.com>
2023-10-11 19:01:57 +0300

    HW2 Task_3

--- src/Homeworks/HW2/Main.java
+++ src/Homeworks/HW2/Main.java
@@ -1,5 +1,7 @@
 package Homeworks.HW2;
 
+import static java.util.Collections.swap;
+
 public class Main {
     public static void main(String[] args) {
         int age = 20;
@@ -19,5 +21,15 @@ public class Main {
         b = a;
         a = c;
         System.out.println("a = " + a + ",\t" + "b = " + b);
+
+        int k = 5;
+        int l = 10;
+        System.out.println("k = " + k + ",\t" + "l = " + l);
+
+        k = k ^ l;
+        l = k ^ l;
+        k = k ^ l;
+        System.out.println("k = " + k + ",\t" + "l = " + l);
+
     }
 }
