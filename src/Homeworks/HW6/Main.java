711df806f05a19dd51f586453a1e18dfec1ab97d
Tymcha <andriyy29.timchuk@gmail.com>
2023-10-26 10:58:42 +0300

    HM Method_1

---
+++ src/Homeworks/HW6/Main.java
@@ -0,0 +1,33 @@
+package Homeworks.HW6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+//        int number = 25;
+        int number = scan.nextInt();
+        int result = sumOfDigits(number);
+        System.out.println("The sum is " + result);
+
+    }
+
+    private static int sumOfDigits(int number) {
+        // Make sure the number is positive
+        number = Math.abs(number);
+
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;  // Get the last digit
+            sum += digit;             // Add the last digit to the sum
+            number /= 10;             // Remove the last digit
+        }
+        return sum;
+    }
+}
+
+
+
+
