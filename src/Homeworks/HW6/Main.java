b0a61e898602b03dc1d3117bf095c8ac2771cc0d
Tymcha <andriyy29.timchuk@gmail.com>
2023-10-26 11:38:56 +0300

    HW Method_2

--- src/Homeworks/HW6/Main.java
+++ src/Homeworks/HW6/Main.java
@@ -11,6 +11,11 @@ public class Main {
         int result = sumOfDigits(number);
         System.out.println("The sum is " + result);
 
+        int resultFid;
+        int n = 11;
+        resultFid = getNumberOffibonacciSeries(n);
+        System.out.println(resultFid);
+
     }
 
     private static int sumOfDigits(int number) {
@@ -25,6 +30,19 @@ public class Main {
             number /= 10;             // Remove the last digit
         }
         return sum;
+    }
+
+
+    private static int getNumberOffibonacciSeries(int n) {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 }
 
