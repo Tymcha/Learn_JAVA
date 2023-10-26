954e1fb6be3a7b6aaf9d1e89e5d91b99b7e8ae21
Tymcha <andriyy29.timchuk@gmail.com>
2023-10-26 15:08:41 +0300

    HW Method_2

---
+++ src/Homeworks/HW7/Main.java
@@ -0,0 +1,13 @@
+package Homeworks.HW7;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+
+        Point point = new Point(x, y);
+        System.out.println(point.getX());
+        System.out.println(point.getY());
+
+    }
+}
